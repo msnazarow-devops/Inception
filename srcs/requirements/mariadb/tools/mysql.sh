4c801e1a6fc64dc597b32e704dfc50e5a4a4bd19
msnazarow <msnazarow@gmail.com>
2021-08-01 21:20:42 +0300

    root_password_final fix

--- srcs/requirements/mariadb/tools/mysql.sh
+++ srcs/requirements/mariadb/tools/mysql.sh
@@ -3,11 +3,11 @@ openrc default
 service mariadb setup
 service mariadb start
 sleep 1
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
+mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
 mysql -u root -p$MYSQL_ROOT_PASSWORD -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE"
-mysql -u root -p$MYSQL_ROOT_PASSWORD -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
+mysql -u root -p$MYSQL_ROOT_PASSWORD -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD';"
 mysql -u root -p$MYSQL_ROOT_PASSWORD -e "CREATE USER IF NOT EXISTS 'sam'@'%' IDENTIFIED BY 'sam';"
 mysql -u root -p$MYSQL_ROOT_PASSWORD -e "GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' WITH GRANT OPTION;"
-mysql -u root -p$MYSQL_ROOT_PASSWORD -e "FLUSH PRIVILEGES"
+mysql -u root -p$MYSQL_ROOT_PASSWORD -e "FLUSH PRIVILEGES;"
 service mariadb stop
 mysqld_safe
